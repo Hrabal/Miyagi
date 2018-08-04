63a23046fd2c72cd46cd1bfce56238d400cbe10f
Hrabal <federicocerchiai@gmail.com>
2018-08-04 12:22:32 +0200

    jsonapi refactor

--- web/apis/jsonapi.py
+++ web/apis/jsonapi.py
@@ -1,34 +1,35 @@
 from vibora.responses import JsonResponse
 
 from ...objects import TypedMany
-from ..web import MiyagiRoute
+from ..web import MiyagiRoute, MiyagiBlueprint
 from ...miyagi import App
 
 
-class JsonApiHandlers:
-    def __init__(self, obj, app: App):
-        self.obj = obj
-        self.app = app
+class JsonApi(MiyagiBlueprint):
+    base_path = '/jsonapi'
 
     def craft(self):
-        for handler_factory in (self.base_collection, ):
-            yield from handler_factory()
+        for proc_name, process in self.app.processes.items():
+            for obj in process.objects:
+                if obj._json_api:
+                    for handler_factory in (self.base_collection, ):
+                        yield from handler_factory(obj)
 
-    def base_collection(self):
-        uri = '/'.join(part.name.lower() for part in self.obj.path)
+    def base_collection(self, obj):
+        uri = f'{self.base_path}/{"/".join(part.name.lower() for part in obj.path)}'
 
         async def base_collection_blueprint(app: App):
             return JsonResponse({
                 "links": {
-                    "self": f"{self.app.webapp.url_scheme}://{self.app.config.host}/jsonapi/{uri}"
+                    "self": f"{self.app.webapp.url_scheme}://{self.app.config.host}/{uri}"
                 },
                 "data": [{
-                    "type": self.obj.name,
+                    "type": obj.name,
                     "id": resource.uid,
                     "attributes": {
                         k: v for k, v in resource.items() if not isinstance(v, TypedMany)
                     }
-                } for resource in app.db.query(self.obj.cls).all()]
+                } for resource in app.db.query(obj.cls).all()]
             })
-        base_collection_blueprint.__name__ = f'{self.obj.name.lower()}_collection'
-        yield MiyagiRoute(f'/{uri}', ['GET', ], base_collection_blueprint)
+        base_collection_blueprint.__name__ = f'{obj.name.lower()}_collection'
+        yield MiyagiRoute(f'{uri}', ['GET', ], base_collection_blueprint)
--- web/web.py
+++ web/web.py
@@ -1,6 +1,13 @@
+from ..miyagi import App
+
 
 class MiyagiRoute:
     def __init__(self, uri: str, methods: list, fnc):
         self.uri = uri
         self.methods = methods
         self.handler = fnc
+
+
+class MiyagiBlueprint:
+    def __init__(self, app: App):
+        self.app = app
